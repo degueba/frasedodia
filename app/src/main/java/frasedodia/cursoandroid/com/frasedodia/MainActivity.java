736f2a2f8911b8dff713c0b4d2d69436cdaf628b
Rodrigo Botelho <rodrigo.botelho@innersite.com.br>
2017-04-24 18:20:28 -0300

    Segundo aplicativo feito em android - Frases do dia

---
+++ app/src/main/java/frasedodia/cursoandroid/com/frasedodia/MainActivity.java
@@ -0,0 +1,49 @@
+package frasedodia.cursoandroid.com.frasedodia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textoNovaFrase;
+    private Button botaoNovaFrase;
+    private String[] frases =  {
+            "As pessoas costumam dizer que a motivação não dura sempre. Bem, nem o efeito do banho, por isso recomenda-se diariamente.",
+            "Motivação é a arte de fazer as pessoas fazerem o que você quer que elas façam porque elas o querem fazer.",
+            "Toda ação humana, quer se torne positiva ou negativa, precisa depender de motivação.",
+            "A verdadeira motivação vem de realização, desenvolvimento pessoal, satisfação no trabalho e reconhecimento.",
+            "Eu faço da dificuldade a minha motivação. A volta por cima, vem na continuação.",
+            "Não deixe que as pessoas te façam desistir daquilo que você mais quer na vida. Acredite. Lute. Conquiste. E acima de tudo, seja feliz!",
+            "A persistência é o caminho do êxito."
+    };
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textoNovaFrase = (TextView) findViewById(R.id.textoNovaFraseId);
+        botaoNovaFrase = (Button) findViewById(R.id.botaoNovaFraseId);
+
+
+        botaoNovaFrase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Random randomico = new Random();
+                int numeroAleatorio = randomico.nextInt( frases.length );
+
+                textoNovaFrase.setText(frases[numeroAleatorio]);
+            }
+        });
+
+
+    }
+}
